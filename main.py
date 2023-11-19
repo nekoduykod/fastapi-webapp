ab65fd936684d4846730ab2b4e9f1cab2d489f84
nekoduykod <zaliubovskyio@ukr.net>
2023-11-19 23:22:46 +0200

    home login register

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates 
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from dotenv import load_dotenv
 import os
@@ -15,20 +17,33 @@ from schema import Statistics as SchemaStatistics
 load_dotenv(".env")
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):    # DO I NEED ASYNC HERE and next snippets
+    return templates.TemplateResponse("home.html", {"request": request})
 
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
+@app.get('/login', response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get('/register', response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 @app.post("/add-user/", response_model=SchemaUsers)
-def register_user(user: SchemaUsers):
+def add_user(user: SchemaUsers):
     db_user = ModelUsers(id=user.id, username=user.username, password=user.password, email=user.email)
     db.session.add(db_user)
     db.session.commit()
     return db_user
+
+@app.get("/users/")
+def get_users():
+    users = db.session.query(Users).all()
+    return users
 
 @app.post("/add-stat/", response_model=SchemaStatistics)
 def add_statistic(stat: SchemaStatistics):
@@ -39,7 +54,7 @@ def add_statistic(stat: SchemaStatistics):
     db.session.commit()
     return db_statistics
 
-@app.get("/users/")
-def get_users():
-    users = db.session.query(Users).all()
-    return users
+@app.get("/stats/")
+def get_stat():
+    stats = db.session.query(Statistics).all()
+    return stats
--- models.py
+++ models.py
@@ -8,8 +8,8 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    username = Column(String, unique=True)
+    password = Column(String(255))
     email = Column(String, unique=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
