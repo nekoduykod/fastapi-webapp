37a2b310488fac71bf27ad2bb4e3aa08b10fe2f0
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2023-12-19 20:05:36 +0200

    Updated schema.py

--- schema.py
+++ schema.py
@@ -15,13 +15,13 @@ class Site(BaseModel):
     class Config:
        orm_mode = True
 
-class Statistics(BaseModel):
-    id: int   
-    user_id: int   
-    page_transitions: int 
-    vpn_site_transitions: int
-    data_sent: int
-    data_received: int
+# class Statistics(BaseModel):
+#     id: int   
+#     user_id: int   
+#     page_transitions: int 
+#     vpn_site_transitions: int
+#     data_sent: int
+#     data_received: int
     
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
