60944f30bf732ab09898ae21c0378dcf6470e767
Oleksandr <zaliubovskyio@ukr.net>
2024-01-16 00:13:26 +0200

    this is for swagger ui of FastAPI

---
+++ junk code/swagger.py
@@ -0,0 +1,26 @@
+''' Swagger UI http://127.0.0.1:8000/docs#/ '''
+from models import Users as ModelUsers
+
+from schema import Users as SchemaUsers
+
+from fastapi_sqlalchemy import db
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/add-user/", response_model=SchemaUsers)
+def add_user(user: SchemaUsers):
+    db_user = ModelUsers(username=user.username, password=user.password, email=user.email)
+    db.session.add(db_user)
+    db.session.commit()
+    return db_user
+
+@app.get("/users/")
+def get_users():
+    users = db.session.query(ModelUsers).all()
+    return users
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
