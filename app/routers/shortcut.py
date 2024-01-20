777861343c2e5c6230d63c451f5cf67a6451d122
Oleksandr <zaliubovskyio@ukr.net>
2024-01-20 23:31:51 +0200

    cosmetic

--- app/routers/shortcut.py
+++ app/routers/shortcut.py
@@ -13,35 +13,33 @@ templates = Jinja2Templates(directory="templates")
 
 @router.post("/create-shortcut")
 async def create_shortcut(request: Request,
-                   shortcut_title: str = Form(...),
-                     shortcut_url: str = Form(...)):
-  user = request.session.get("user")
-  if user:
-     shortcut = ModelShortcuts(title=shortcut_title, url=shortcut_url, user_id=user["id"])
-     db.session.add(shortcut)
-     db.session.commit()
-     return RedirectResponse(url="/account", status_code=303)
-  else:
-     return RedirectResponse("/account")
+                shortcut_title: str = Form(...),
+                  shortcut_url: str = Form(...)):
+     user = request.session.get("user")
+     if user:
+         shortcut = ModelShortcuts(title=shortcut_title, url=shortcut_url, user_id=user["id"])
+         db.session.add(shortcut)
+         db.session.commit()
+         return RedirectResponse(url="/account", status_code=303)
+     else:
+         return RedirectResponse("/account")
 
 
 @router.delete("/delete-shortcut/{shortcut_id}")
 async def delete_shortcut(request: Request, shortcut_id: int):
-    user = request.session.get("user")
-    if not user:
+     user = request.session.get("user")
+     if not user:
         return RedirectResponse("/login")
-
-    shortcut = db.session.query(ModelShortcuts).filter(
+     shortcut = db.session.query(ModelShortcuts).filter(
         ModelShortcuts.id == shortcut_id,
         ModelShortcuts.user_id == user["id"]
-    ).first()
-
-    if shortcut:
+     ).first()
+     if shortcut:
         db.session.delete(shortcut)
         db.session.commit()
         return {"message": "Shortcut deleted successfully"}
-    else:
-        return {"error": "Shortcut wasn`t created"}
+     else:
+         return RedirectResponse("/account")
     
 
 @router.get("/update-shortcuts")
